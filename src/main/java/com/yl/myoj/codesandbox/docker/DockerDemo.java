0764e9251b314915317489e5f74535b5191106b3
LiYang186 <1443510186@qq.com>
2023-09-17 22:08:44 +0800

    feat:使用java拉取镜像，创建docker容器

---
+++ src/main/java/com/yl/myoj/codesandbox/docker/DockerDemo.java
@@ -0,0 +1,36 @@
+package com.yl.myoj.codesandbox.docker;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.*;
+import com.github.dockerjava.api.model.PullResponseItem;
+import com.github.dockerjava.core.DockerClientBuilder;
+
+public class DockerDemo {
+    public static void main(String[] args) throws InterruptedException {
+        DockerClient dockerClient = DockerClientBuilder.getInstance().build();
+//        PingCmd pingCmd = dockerClient.pingCmd();
+//        pingCmd.exec();
+        String image ="nginx:latest";
+        //拉取镜像
+//        PullImageCmd pullImageCmd = dockerClient.pullImageCmd(image);
+//        PullImageResultCallback pullImageResultCallback = new PullImageResultCallback(){
+//            @Override
+//            public void onNext(PullResponseItem item) {
+//                System.out.println("下载镜像："+item.getStatus());
+//                super.onNext(item);
+//            }
+//        };
+//        pullImageCmd
+//                .exec(pullImageResultCallback)
+//                .awaitCompletion();
+//        System.out.println("下载完成");
+
+        //创建容器
+        CreateContainerCmd containerCmd = dockerClient.createContainerCmd(image);
+        CreateContainerResponse createContainerResponse = containerCmd
+                .withCmd("echo","Hello Docker")
+                .exec();
+        System.out.println(createContainerResponse);
+
+    }
+}
--- src/main/resources/testCode/simpleComputeArgs/Main.java
+++ src/main/resources/testCode/simpleComputeArgs/Main.java
@@ -4,6 +4,6 @@ public class Main
 {
     public static void main(String args[]) throws Exception
     {
-        Thread.sleep(10000);
+        System.out.println("hhh");
     }
 }
